b3773d5e9c25872a76536efa91d844b367150a59
Ivan Surzhenko <ivan.surzhenko@dev-pro.net>
2019-09-29 13:18:17 +0300

    Temporary switched to use local sample.json

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-swagger-codegen generate -i https://developers.salesloft.com/v2_api_def.json -l typescript-fetch -o . -t ./codegen_template/typescript-fetch/
+swagger-codegen generate -i sample.json -l typescript-fetch -o . -t ./codegen_template/typescript-fetch/
